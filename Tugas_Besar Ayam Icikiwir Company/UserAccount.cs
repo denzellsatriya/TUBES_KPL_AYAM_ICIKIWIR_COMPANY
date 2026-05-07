e6cd43934c9d5062c54ed7df0c7182945c16d29d
Denzell Satriya Putera Wibisono <denzellsatriya@gmail.com>
2026-05-08 00:21:22 +0700

    runtime config (tambah class userAccount & json userConfig)

---
+++ Tugas_Besar Ayam Icikiwir Company/UserAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Tugas_Besar_Ayam_Icikiwir.Models
+{
+    //runtime config
+    public class UserAccount
+    {
+        //staff
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        //pengunjung
+        public string NomorIdentitas { get; set; } = string.Empty; 
+        public string Nama { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = "Pengunjung";
+
+        public void ValidasiDataPengunjung()
+        {
+            if (string.IsNullOrWhiteSpace(Nama))
+                throw new ArgumentException("Nama harus diisi!");
+
+            if (NomorIdentitas.Length < 5 || !NomorIdentitas.All(char.IsDigit))
+                throw new ArgumentException("Nomor Identitas (NIM/NIK) harus berupa angka minimal 5 digit!");
+
+            if (!Email.Contains("@"))
+                throw new ArgumentException("Format email tidak valid!");
+        }
+    }
+}
--- Tugas_Besar Ayam Icikiwir Company/obj/Debug/net10.0/Tugas_Besar Ayam Icikiwir Company.AssemblyInfo.cs
+++ Tugas_Besar Ayam Icikiwir Company/obj/Debug/net10.0/Tugas_Besar Ayam Icikiwir Company.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tugas_Besar Ayam Icikiwir Company")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a7ab7c7ceb276df1b7fe547d4f3ef1d46e52094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a6a8783f2fd90404e1b25cde62ab438e3027ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tugas_Besar Ayam Icikiwir Company")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tugas_Besar Ayam Icikiwir Company")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
