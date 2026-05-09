77d5215f483dd1dda1a54fff1ad8bfa218bd4204
Moldy-Cheese <shevaibrahim.xpro@gmail.com>
2026-05-09 17:45:29 +0700

    add library logs

---
+++ Tugas_Besar Ayam Icikiwir Company/Iloggable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tugas_Besar_Ayam_Icikiwir.Models
+{
+    public interface ILoggable
+    {
+        List<LogEntry> Logs { get; set; }
+    }
+
+    public class LogEntry
+    {
+        public DateTime Waktu { get; set; }
+        public string Aksi { get; set; } = "";
+        public string OlehSiapa { get; set; } = ""; 
+        public string Keterangan { get; set; } = ""; 
+    }
+}
---
+++ Tugas_Besar Ayam Icikiwir Company/Itanggaldibuat.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Tugas_Besar_Ayam_Icikiwir.Models
+{
+    public interface ITanggalDibuat
+    {
+        DateTime TanggalDibuat { get; set; }
+    }
+}
