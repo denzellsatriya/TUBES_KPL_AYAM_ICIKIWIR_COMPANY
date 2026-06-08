d2d258be8c7d5fbef86d28b8719496f0efce8717
Moldy-Cheese <shevaibrahim.xpro@gmail.com>
2026-06-08 21:56:14 +0700

    code clean 4

--- Tugas_Besar_Ayam_Icikiwir_Company/Models/BukuCreateDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tugas_Besar_Ayam_Icikiwir_Company.Models
-{
-    public class BukuCreateDTO
-    {
-        public string Judul { get; set; } = "";
-        public DateTime TanggalDibuat { get; set; }
-    }
-}
